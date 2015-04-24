7d708efb20af65d5746dc449a7753d74278f8191
Yigong Wang <yigwang@cs.stonybrook.edu>
2015-04-23 23:33:49 -0400

    trivial modifications

--- bin/user_1/user_1.c
+++ bin/user_1/user_1.c
@@ -42,7 +42,6 @@ int main(int argc, char* argv[], char* envp[])
 	//	j = 1000000;
 		//if(i % 5 == 0)
 		      i = i + 3;
-			  while(1);
  //                      yield(1);
 	}
 	return 0;
--- sys/main.c
+++ sys/main.c
@@ -85,10 +85,9 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 	// now, Dongju disable timer interrupt because it makes me to debug difficult.
 	// If you want to enable timer interrupt, add ENABLE_TIMER_INT with '|'
 	set_timer(1000);
-	round_robin_scheduler();
-
 	__asm volatile("sti");// enable interupt("asm sti") should be executed after setting all interrupt info.
 
+	round_robin_scheduler();
 
 	while (1);
 
