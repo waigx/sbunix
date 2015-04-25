298ef31ee81c9a4d9851116509cdbf5787d2398f
Yigong Wang <yigwang@cs.stonybrook.edu>
2015-04-25 01:52:40 -0400

    add missed testtask

---
+++ sys/scheduler/testtasks.c
@@ -0,0 +1,48 @@
+/*
+ *
+ *  This file is part of sbunix.
+ *        sbunix is a simple linux implementation, basically, it is
+ *  an academic project of CSE506 of Stony Brook University in Spring 
+ *  2015. For more details, please refer to README.md.
+ *
+ *  Copyright (C) 2015 Dongju Ok   <dongju@stonybrook.edu,
+ *                                  yardbirds79@gmail.com>
+ *  Copyright (C) 2015 Yigong Wang <yigwang@cs.stonybrook.edu>
+ * 
+ *
+ *  sbunix is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  sbunix is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with sbunix.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <sys/sched/sched.h>
+#include <sys/sbunix.h>
+#include <sys/debug.h>
+
+#include <sys/managemem.h>
+
+
+
+
+void load_test_tasks(void)
+{
+	task_t *task1;
+	task_t *task2;
+
+	task1 = newtask("bin/user_1", USER_PROCESS);
+	task2 = newtask("bin/user_2", USER_PROCESS);
+
+	loadtask(task1);
+	loadtask(task2);
+
+}
